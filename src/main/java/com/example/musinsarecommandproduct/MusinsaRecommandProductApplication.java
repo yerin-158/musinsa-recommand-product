3641a1440da6eb65f04332592a7c4dab0d721d8b
Son Yerin <hanblueblue@gmail.com>
2024-06-30 14:57:53 +0900

    [MUSINSA-14] Spring Boot 3 프로젝트 셋팅

---
+++ src/main/java/com/example/musinsarecommandproduct/MusinsaRecommandProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.musinsarecommandproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaRecommandProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MusinsaRecommandProductApplication.class, args);
+  }
+
+}
