a9f8229a2b27eaddbef70ed8db7bdb82baff8fe0
Son Yerin <hanblueblue@gmail.com>
2024-07-03 13:31:51 +0900

    [MUSINSA-79] [api] 어드민 스웨거 셋팅 및 점검

--- src/main/java/com/example/musinsarecommandproduct/controller/store/interfaces/CategoryApi.java
+++ src/main/java/com/example/musinsarecommandproduct/controller/store/interfaces/CategoryApi.java
@@ -1,6 +1,12 @@
 package com.example.musinsarecommandproduct.controller.store.interfaces;
 
+import com.example.musinsarecommandproduct.controller.store.dto.CategorySimpleResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Created by yerin-158 on 7/3/24.
@@ -11,4 +17,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
  */
 @Tag(name = "Category API", description = "카테고리 API")
 public interface CategoryApi {
+
+  @GetMapping
+  @Operation(summary = "카테고리 리스트 가져오기")
+  ResponseEntity<List<CategorySimpleResponse>> getCategories();
+
 }
